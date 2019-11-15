b4d4ef9fb4ff95462446fbc388cc3fd13ca3b91d
NikhilKommuri <nikhil.kommuri@gmail.com>
2019-11-15 18:40:42 +0000

    Added Print Statements

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def centuryFromYear(year):
     rem =  year%100
     quo =  int(year/100)
     if(rem == 0):
+        print(quo)
         return quo
     else:
+        print(quo)
         return quo + 1
         
 centuryFromYear(1905)  
