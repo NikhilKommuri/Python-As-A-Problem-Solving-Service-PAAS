02391c80aae7740a131dc5a5d3cae9220e12906a
Nikhil Kommuri <44361996+NikhilKommuri@users.noreply.github.com>
2020-06-03 11:57:24 -0400

    Update MaxSubArray.py

--- MaxSubArray.py
+++ MaxSubArray.py
@@ -11,3 +11,19 @@ def maxsumsubarray(arr):
     return  global_max_sum
 
 #print(maxsumsubarray(arr))
+
+'''
+Another Way to do it but only for +ve sum
+nums = [-2,-3,4,-1,-2,1,5,-3]
+def kadane(nums):
+    csum = nums[0]
+    osum = nums[0]
+    for i in range(1,len(nums)):
+        csum = csum + nums[i]
+        if csum < 0:
+            csum = 0
+        osum = max(osum,csum)
+    return osum
+#print
+
+'''
